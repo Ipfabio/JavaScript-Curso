b4973c0a9471548176d96c40da0c01a8ef577b0e
Fabio <fabioperrone95@hotmail.com>
2023-03-12 20:08:48 -0300

    Tipos

---
+++ Segunda Parte/iniciacion-tipos.js
@@ -0,0 +1,24 @@
+// Tipos primitivos
+
+// number
+4;
+
+// string (también comillas de lado)
+("Hola mundo");
+("Hola mundo");
+
+// booleanos
+true;
+false;
+
+// nulo y undefined
+null;
+undefined;
+
+// null, undefined, false, 0 ==> Todas son Falsy
+console.log(null === undefined);
+if (true) {
+  console.log("cumple");
+} else {
+  console.log("no cumple");
+}
