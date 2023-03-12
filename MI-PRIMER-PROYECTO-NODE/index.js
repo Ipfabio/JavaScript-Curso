d4170c39cf0656e16307120609ae10accd1f4ffc
Fabio <fabioperrone95@hotmail.com>
2023-03-12 19:27:26 -0300

    Introduccion a JS

---
+++ MI-PRIMER-PROYECTO-NODE/index.js
@@ -0,0 +1,13 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto!")
+
+/*
+Esta es la parte intermedi de nuestro proyecto
+En esta parte realizamos los chequeos necesarios
+Por último vamos a la úlima línea de saludo
+Esto también está comentado
+aquí también
+*/
+
+// Este va a ser el mensaje que se envia al despedir
+console.log("Adios!")
