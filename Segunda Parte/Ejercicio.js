393f592a7e45a880726b1d5a41a7599c1acd459e
Fabio <fabioperrone95@hotmail.com>
2023-03-14 17:42:57 -0300

    Agrego el enunciado del ejercicio

--- Segunda Parte/Ejercicio.js
+++ Segunda Parte/Ejercicio.js
@@ -12,3 +12,17 @@ const alumno = {
 };
 
 console.log(alumno.fecha_nacimiento);
+
+/*Enunciado del ejercicio:
+
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) */
