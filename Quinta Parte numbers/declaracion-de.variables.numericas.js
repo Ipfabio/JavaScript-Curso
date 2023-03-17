ff86a3c6f1e5e9898df71285a1c6edc8290283c1
Fabio <fabioperrone95@hotmail.com>
2023-03-17 15:01:18 -0300

    Parte 5 numbers

---
+++ Quinta Parte numbers/declaracion-de.variables.numericas.js
@@ -0,0 +1,16 @@
+// Sesión 05 - NUmbers
+// Declaración de variables númericas (enteros y con decimales)
+let a = 5;
+
+console.log(a);
+
+let b = 0.1;
+
+console.log(b);
+
+// Precisión
+let c = 0.2;
+
+console.log(b + c);
+// Pequeño truco para obtener valores "reales"
+console.log(Math.round((0.1 + 0.2) * 100) / 100);
