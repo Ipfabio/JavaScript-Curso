3668d5854bf25d8047d6034be19b1cfd18b64558
Fabio <fabioperrone95@hotmail.com>
2023-03-17 16:55:37 -0300

    Ejercicio quinta parte

---
+++ Quinta Parte numbers/ejercicio/ejercicio.js
@@ -0,0 +1,14 @@
+let altura_centimetros = 170;
+console.log(altura_centimetros);
+
+let altura_metros = 1.7;
+console.log(altura_metros.toFixed(2));
+
+let peso_kg = 70.0;
+console.log(peso_kg);
+
+let altura_mts_redondeado = 1.7;
+console.log(altura_mts_redondeado.toFixed());
+
+let peso_kg_redondeado = 70;
+console.log(peso_kg_redondeado.toFixed());
